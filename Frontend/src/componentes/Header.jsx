ad5bd70b589a4b444107263be1f39336f53987c5
Emmow <101145147+FrankRojas31@users.noreply.github.com>
2023-10-31 14:06:25 +0000

    Se agrega audio en el Header

--- Frontend/src/componentes/Header.jsx
+++ Frontend/src/componentes/Header.jsx
@@ -91,6 +91,11 @@ export default function Header() {
           <></>
         )}
       </nav>
+      <audio controls autoplay>
+        <source src="horse.ogg" type="audio/ogg" />
+        <source src="horse.mp3" type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
     </>
   );
 }
