ce82c91c8365433b734c29611e35929a01911d8f
Alonso <carlosroano201@gmail.com>
2023-11-06 21:54:11 -0500

    mejora del index

--- Frontend/src/Paginas/index.jsx
+++ Frontend/src/Paginas/index.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Header from "../componentes/Header";
 import Footer from "../componentes/Footer";
+import "../css/inicio.css"
+import AudioPlayer from "../componentes/audioplayer";
 export default function Index() {
     const [cancion, setCancion] = useState("");
     const [canciones, setCanciones] = useState([]);
 
     function handleSearch (){
-        e.preventDefault();
         if(cancion.trim()==="") {
             alert("Debes ingresar algo");
             return;
@@ -27,10 +28,10 @@ export default function Index() {
 
     async function getSong(cancion) {
         try {
+            let url=`https://spotify23.p.rapidapi.com/search/?q=${cancion}&type=multi&offset=0&limit=10&numberOfTopResults=5`;
             let data = await fetch(url, options);
             let res = await data.json();
             setCanciones(res.tracks.items);
-            let url=`https://spotify23.p.rapidapi.com/search/?q=${cancion}&type=multi&offset=0&limit=10&numberOfTopResults=5;            `
         } catch (error) {
             console.log(`Upsss... ha ocurrido un error: ${error}`);
         }
@@ -41,6 +42,13 @@ export default function Index() {
     return (
         <>
             <Header></Header>
+            <AudioPlayer></AudioPlayer>
+            <div className="banner">
+                <div className="banner-texto">
+                <h1 className="h1">MELOMIX</h1>
+                <p className="p">*¡La mejor música, ahora a tu alcance!</p>
+                </div>
+            </div>
             <main className="w-full min-h-screen grid grid-cols-1 lg:grid-cols-8">
                 <section className="col-span-6 w-full h-full flex flex-wrap justify-center p-[20px]">
                     <h1 className="row-span-1 text-white text-[35px] font-mono text-center">DISFRUTA DE LA MEJOR MUSICA EN ¡MELO MIX!</h1>
@@ -54,7 +62,7 @@ export default function Index() {
                         <span className="flex h-[50px] justify-center w-full">
                             <input type="text" className="h-full p-3 border-none outline-none rounded-l-xl w-full" value={cancion} onChange={e =>  setCancion(e.target.value)}/>
                             <div className="bg-black rounded-xl px-3 h-full flex justify-center items-center -ml-[20px] px-5">
-                                <i className="nf nf-fa-search text-white font-medium text-xl" onClick={e => handleSearch(e)}></i>
+                                <i className="nf nf-fa-search text-white font-medium text-xl" onClick={handleSearch}></i>
                                 </div>
                         </span>
                     </div>
--- Frontend/src/componentes/Header.jsx
+++ Frontend/src/componentes/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import AudioPlayer from "./audioplayer";
 
 export default function Header() {
   const navigate = useNavigate();
@@ -94,7 +93,6 @@ export default function Header() {
           <></>
         )}
       </nav>
-      <AudioPlayer></AudioPlayer>
     </>
   );
 }
